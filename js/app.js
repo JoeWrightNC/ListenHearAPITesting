02e3b8128752e5504b444a09b9d1d8d1b860cafb
Joe Wright <joewrightart@gmail.com>
2018-04-07 22:20:02 -0400

    spotify player testing

--- js/app.js
+++ js/app.js
@@ -1,90 +1,240 @@
+var fakeData = [
+  {
+    name: "Cowboy Dude",
+    genre: "country",
+    venue: "303 Country Venue Ln."
+  },
+  {
+    name: "Ranch Gal",
+    genre: "country",
+    venue: "303 Country Venue Ln."
+  },
+  {
+    name: "Deathmetal Dave",
+    genre: "metal",
+    venue: "14 Warehouse Rd."
+  },
+  {
+    name: "Dustin Beaver",
+    genre: "pop",
+    venue: "100 Main St."
+  },
+  {
+    name: "DJ Bingo",
+    genre: "trance",
+    venue: "14 Warehouse Rd."
+  },
+  {
+    name: "Frankie Lovejoy",
+    genre: "rock",
+    venue: "100 Main St."
+  }
+];
 
-function pingSeatGeek() {
+let colors = ['#1be3c9', '#6abd75', '#b986e0', '#32f57d', '#eccd52', '#f198b2'];
+
+let session = {
+  zip: '',
+  radius: '',
+  EVENT_ARR: [],
+}
+
+//HELPER FUNCTIONS
+
+function zipError(errorMessage) {
+  $('#zip').val(errorMessage).css('color', 'red');
+  setTimeout(function () {
+    $('#zip').val("enter zip").css('color', 'black');
+  }, 2000);
+};
+
+function radiusError(errorMessage) {
+  $('#radius').val(errorMessage).css('color', 'red');
+  setTimeout(function () {
+    $('#radius').val("enter zip").css('color', 'black');
+  }, 2000);
+};
+
+function validateInput() {
+  let zip = $("#zip").val().trim();
+  let radius = $("#radius").val().trim();
+  console.log(zip);
+  console.log(radius);
+  //handle zip
+  if (zip.length != 5) {
+    zipError("enter 5 digit zip");
+    console.log("zip length err");
+    return false;
+  }
+  for (let i = 0; i < zip.length; i++) {
+    if ( isNaN(zip.charAt(i)) ) {
+      zipError("invalid zip");
+      console.log("zip err");
+      return false;
+    }
+  }
+  //handle radius
+  if (radius == ""){
+    radiusError("enter a number");
+    return false;
+  }
+  for (let j = 0; j < radius.length; j++) {
+    console.log(radius.charAt(j))
+    if ( isNaN(radius.charAt(j)) ) {
+      radiusError("enter a number");
+      console.log("radius err");
+      return false;
+    }
+  }
+  //set session vars & return true if conditions met
+  session.zip = zip;
+  session.radius = radius;
+  return true;
+}
+
+// API functions
+
+function callSpotify() {
+  let accessToken = "BQDrNxu74lLL30iISaOsqUSbyVYTlZk8_nZJ9GCf7hcqET6tskzxCBMQn5aMaxBCNVNDG6YcD5CEfReGhXexqtUN7RutIdp3QPc7RqlRPV2YoFeaZXMDlsR4Pc-r_UNGVfBZch2LMcS259sfmMCpqLMbJTDQd_XQ3pPXJmPA99k8mf2A9g"
+  //optionally: use forEach?
+  for (let i = 0; i < session.EVENT_ARR.length; i++) {
+    $.ajax({
+      url: "https://api.spotify.com/v1/search?q=" + session.EVENT_ARR[i].artist + "&type=artist",
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      success: function (response) {
+        console.log(response);
+        let spotifyId = response.artists.items["0"].id;
+        console.log(spotifyId)
+        session.EVENT_ARR[i].spotifyId = spotifyId
+        let genres = response.artists.items["0"].genres;
+        genres.forEach(genre =>{
+          session.EVENT_ARR[i].genres.push(genre);  
+        });
+      }
+    });
+  };
+}
+
+function callSeatGeek() {
   event.preventDefault();
-  ZIP = $("#zipcodeInput").val().trim();
-  DIST = $("#distInput").val().trim();
-  DATE = moment().format("YYYY-MM-DD");
-  console.log(DATE)
+  const ZIP = session.zip;
+  const DIST = session.radius;
+  const DATE = moment().format("YYYY-MM-DD");
+  console.log(DATE, " ", ZIP, " ", DIST);
   var queryURL = "https://api.seatgeek.com/2/events?type=concert&per_page=1000&postal_code=" + ZIP + "&range=" + DIST + "mi&client_id=MTExMzAwNzR8MTUyMjk4NDcwNS4xNg"
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
-    console.log(response)
+  }).then(function (response) {
+    //clear session on new search
+    session.EVENT_ARR = [];
+    //optionally: use forEach?
     for (var i = 0; i < response.events.length; i++) {
-      var timeChecker = response.events[i].datetime_local
-      var timeCheckerTwo = moment(timeChecker).format("YYYY-MM-DD")
+      let timeChecker = response.events[i].datetime_local
+      let timeCheckerTwo = moment(timeChecker).format("YYYY-MM-DD")
       if (timeCheckerTwo == DATE) {
-      $("#artistsDiv").append(
-        `
-        <div class="tile col resultCard">
-            <p class="tileTitle">${response.events[i].performers[0].name} | <i>${response.events[i].venue.name}</i></p>
+        //build event array of artists/venues
+        let artistName = response.events[i].performers[0].name
+        let venueName = response.events[i].venue.name
+        let event = {};
+        event.artist = artistName;
+        event.venue = venueName;
+        event.genres = [];
+        event.spotifyId = ""
+        session.EVENT_ARR.push(event);
+        /* $("#artistsDiv").append(
+          `
+          <div class="tile col resultCard">
+              <p class="tileTitle">${response.events[i].performers[0].name} | <i>${response.events[i].venue.name}</i></p>
+            </div>
           </div>
         </div>
-      </div>
-      `
-      )}
-    } 
+        `
+        )} */
+      }
+    }
+  callSpotify();
   })
 }
 
-function pingSpotify() {
-  console.log("pingStarted")
-  // Get the hash of the url
-const hash = window.location.hash
-.substring(1)
-.split('&')
-.reduce(function (initial, item) {
-  if (item) {
-    var parts = item.split('=');
-    initial[parts[0]] = decodeURIComponent(parts[1]);
-  }
-  return initial;
-}, {});
-window.location.hash = '';
 
-// Set token
-let _token = hash.access_token;
+//WORKFLOW
 
-const authEndpoint = 'https://accounts.spotify.com/authorize';
-
-// Replace with your app's client ID, redirect URI and desired scopes
-const clientId = '5d271f5f1df9427cbd92da2e6241045d';
-const redirectUri = 'https://joewrightnc.github.io/ListenHearAPITesting/';
-const scopes = [
-  'user-top-read'
-];
-
-// If there is no token, redirect to Spotify authorization
-if (!_token) {
-  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+function load() {
+  bounceIn("#landing-page");
+  //get location/prompt for location?
 }
 
-// Make a call using the token
-$.ajax({
-   url: "https://api.spotify.com/v1/me/top/artists",
-   type: "GET",
-   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
-   success: function(response) { 
-     // Do something with the returned data
-     console.log(response)
-     }
+$("#launch-button").on("click", function () {
+  let proceed = validateInput();
+  if (proceed) {
+    callSeatGeek();
+    //bounceOut("#landing-page")
+    //bounceIn('#sort-page');
+  }
+})
+
+function sortGenre() {
+  $('.grid').isotope({
+    filter: '.country'
+  });
+};
+
+$('#sort-page').on('click', '#genre-filter', function () {
+  sortGenre();
 });
 
+function loadEvents() {
+  $("#event-container").html("");
+  session.EVENT_ARR.forEach(event => {
+    let randomNum = Math.round(Math.random() * colors.length);
+    let color = colors[randomNum];
+    console.log(event.spotifyId)
+    let index = session.EVENT_ARR.indexOf(event);
+    var html =
+      `
+      <div id="datum-name-${index}">${event.artist}</div>
+      <div id="datum-venue-${index}">${event.venue}</div>
+      <iframe src="https://open.spotify.com/embed/artist/${event.spotifyId}"
+              width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    `;
+    let eventTile = $("<div>")
+      .attr('id', 'event-wrapper' + index)
+      .addClass('event-wrapper grid-item')
+      .addClass(event.genres)
+      .css('background-color', color)
+      .html(html);
+
+    $("#event-container").append(eventTile);
+  });
+};
+
+$("#get-data").on('click', function () {
+  loadEvents();
+});
+
+//DISPLAY
+
+//transition in section
+function bounceIn(section) {
+  setTimeout(function () {
+    $(section).addClass("bounceInDown")
+      .css("display", "block")
+  }, 600);
 }
-/*   var accessToken = "BQANNJyFnwB_kQxvYnD5WM7I1Sscd-tHoFNE_nZGOcK5rl8HxE7WTHatSHimCQHyzWKs4bkzSlRXU3Ba3NmCNzocIktjkOysZBkeGh_jXtx50UGxueQsUXOH4RugPEAle7_XaBsOZuZywyYidUp5VOroBStMPRTyoDyBN-HQR9ROyfs"
-  $.ajax({
-    url: 'https://api.spotify.com/v1/search?q=Sarah%20Shook&type=artist',
-    headers: {
-        'Authorization': 'Bearer ' + accessToken
-    },
-    success: function(response) {
-        console.log(response)
-    }
-    
- });
-  }; */
 
-$("#submitBtn").on("click", pingSeatGeek);
+//transition out section
+function bounceOut(section) {
+  $(section).removeClass("bounceInDown")
+    .addClass("bounceOutUp");
+  setTimeout(function () {
+    $(section).removeClass("bounceOutUp")
+    $(section).css("display", "none");
+  });
+}
 
-$("#spotifyBtn").on("click", pingSpotify);
+$(document).ready(function () {
+  //load();
+})
